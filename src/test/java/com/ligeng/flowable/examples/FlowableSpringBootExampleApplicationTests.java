7a7c9aae08ff154c15da30220a5d1bc1225bf1d9
geng.li <geng.li@timschina.com>
2023-03-28 16:21:28 +0800

    update

--- src/test/java/com/ligeng/flowable/examples/FlowableSpringBootExampleApplicationTests.java
+++ src/test/java/com/ligeng/flowable/examples/FlowableSpringBootExampleApplicationTests.java
@@ -1,13 +1,35 @@
 package com.ligeng.flowable.examples;
 
+import org.flowable.engine.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+
 @SpringBootTest
 class FlowableSpringBootExampleApplicationTests {
+	@Resource
+	private ProcessEngine processEngine;
+	@Resource
+	RuntimeService runtimeService;
+	@Resource
+	RepositoryService repositoryService;
+	@Resource
+	TaskService taskService;
+	@Resource
+	ManagementService managementService;
+	@Resource
+	IdentityService identityService;
+	@Resource
+	HistoryService historyService;
+//	@Resource
+//	FormService formService;
+	@Resource
+	DynamicBpmnService dynamicBpmnService;
 
 	@Test
 	void contextLoads() {
+		System.out.println(processEngine);
 	}
 
 }
