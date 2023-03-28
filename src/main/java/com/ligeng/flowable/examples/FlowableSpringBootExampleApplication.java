dac066dde462fb6739204a137d287175f546765b
geng.li <geng.li@timschina.com>
2023-03-28 14:30:34 +0800

    init

---
+++ src/main/java/com/ligeng/flowable/examples/FlowableSpringBootExampleApplication.java
@@ -0,0 +1,13 @@
+package com.ligeng.flowable.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowableSpringBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowableSpringBootExampleApplication.class, args);
+	}
+
+}
